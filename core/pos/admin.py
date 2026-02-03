571c91b66f51321b9b8cc88a622b3c6f9e0a6094
Carlos Andrade <reelestudiocuenca@gmail.com>
2026-02-03 14:00:45 -0500

    Edit admin.py

--- core/pos/admin.py
+++ core/pos/admin.py
@@ -4,5 +4,6 @@ from core.pos.models import Product, Category, Client # Los nombres que uses
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Client)
+admin.site.register(Company)
 
 # Register your models here.
