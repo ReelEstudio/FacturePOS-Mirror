48851a0445e38ff4d7d563ba1843dd22ce9a12a1
Carlos Andrade <reelestudiocuenca@gmail.com>
2026-02-03 09:25:45 -0500

    Edit settings.py

--- config/settings.py
+++ config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 # 5. Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
