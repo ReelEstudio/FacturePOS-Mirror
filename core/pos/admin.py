694709ae29a6538295d280e4950ecb5d590e24d0
Carlos Andrade <reelestudiocuenca@gmail.com>
2026-02-03 09:20:49 -0500

    Edit admin.py

--- core/pos/admin.py
+++ core/pos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.pos.models import Product, Category, Client # Los nombres que uses
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Client)
 
 # Register your models here.
