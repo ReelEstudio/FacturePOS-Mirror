3d3b0810d9bb1824f40f7849c9b54805a234ba69
Carlos Andrade <reelestudiocuenca@gmail.com>
2026-02-02 21:31:09 -0500

    Edit wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,3 +1,19 @@
+import os
+import subprocess
+import sys
+
+# Intentar encontrar la ruta de las librerías de Nix dinámicamente
+try:
+    nix_lib_path = subprocess.check_output(['nix-build', '-E', 'with import <nixpkgs> {}; symlinkJoin { name = "libs"; paths = [ pango cairo glib ]; }', '--no-out-link']).decode().strip() + "/lib"
+    if nix_lib_path not in os.environ.get('LD_LIBRARY_PATH', ''):
+        os.environ['LD_LIBRARY_PATH'] = f"{nix_lib_path}:{os.environ.get('LD_LIBRARY_PATH', '')}"
+except:
+    # Si falla lo anterior, usamos la ruta estándar de perfiles de Nixpacks
+    os.environ['LD_LIBRARY_PATH'] = f"/nix/var/nix/profiles/default/lib:{os.environ.get('LD_LIBRARY_PATH', '')}"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
 """
 WSGI config for web project.
 
