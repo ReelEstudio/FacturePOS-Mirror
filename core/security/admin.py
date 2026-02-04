fde9f48ce95a35af9b1f7007c0f2a5085eedc23d
Carlos Andrade <reelestudiocuenca@gmail.com>
2026-02-04 07:42:20 -0500

    Edit admin.py

--- core/security/admin.py
+++ core/security/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.security.models import Module, GroupModule, Group
 
+admin.site.register(Module)
+admin.site.register(GroupModule)
+admin.site.register(Group)
 # Register your models here.
